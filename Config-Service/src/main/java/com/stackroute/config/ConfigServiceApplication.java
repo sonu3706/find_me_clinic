2f4d531b1fe35fd1cae645424c42c609fed2ce95
chandan <chandan.mishra@stackroute.in>
2018-08-28 15:27:42 +0530

    first commit

---
+++ Config-Service/src/main/java/com/stackroute/config/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
---
+++ Report-Service/src/main/java/com/stackroute/report/service/UploadService.java
@@ -0,0 +1,8 @@
+package com.stackroute.report.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UploadService {
+	
+public boolean uploadFile(MultipartFile file);
+}
